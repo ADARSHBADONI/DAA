3e863e11d6cfc8d6d547aaf7840501727ad9226d
Adarsh Badoni <91362584+Theyellowflashminato@users.noreply.github.com>
2022-06-02 22:33:08 +0530

    Update dfs.cpp
    enter the total number of vertices:6
    enter total no of edges 6
    enter source to destination edges:
    0 1
    0 2
    2 5
    5 4
    4 1
    1 3
    enter the starting vertex:0
    0  2  5  4  1  3

--- dfs.cpp
+++ dfs.cpp
@@ -1,1 +1,48 @@
-
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+void addEdge(vector<int>adj[],int src,int des){
+  adj[src].push_back(des);
+  adj[des].push_back(src);
+ }
+void dfs(vector<int>adj[],vector<bool>&vis,int sv){
+   stack<int>st;
+   st.push(sv);
+   vis[sv]=true;
+   while(!st.empty()){
+     int temp=st.top();
+     st.pop();
+     cout<<temp<<"  ";
+     
+     for(auto itr:adj[temp]){
+       if(!vis[itr]){
+       st.push(itr);
+       vis[itr]=true;
+       }
+      }
+    }
+  }
+int main(){
+  int e,v;
+  cout<<"enter the total number of vertices:";
+  cin>>v;
+  cout<<"enter total no of edges";
+  cin>>e ;
+  vector<int>adj[v];
+  vector<bool>visited(v,false);
+  cout<<"enter source to destination edges:"<<endl;
+  for(int i=0;i<e;i++){
+  int a,b;
+  cin>>a>>b;
+  addEdge(adj,a,b);
+}
+ int sv;
+ cout<<"enter the starting vertex:";
+ cin>>sv;
+ for(int i=sv;i<v;i++){
+ if(!visited[i]){
+ dfs(adj,visited,i);
+}
+}
+}
